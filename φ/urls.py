886bb49b0a69315dee99e22efab4f77dfc1c4ebf
= <clockdb@gmail.com>
2021-03-15 10:00:57 -0500

    Gamestop

---
+++ φ/urls.py
@@ -0,0 +1,55 @@
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
+
+app_name = "φ"
+
+
+urlpatterns = [
+    
+    path("", views.index, name="index"),
+
+    path("db/", views.db, name="db"),
+
+    path("qp/", views.qp, name="qp"),
+
+    path("disclaimer/", views.disclaimer, name="disclaimer"),
+
+    path("entities/", views.entities, name="entities"),
+    
+    path("documentation/", views.documentation, name="documentation"),
+
+    path("menu/", views.menu, name="menu"),
+
+    path("<str:entity_TradingSymbol>/", views.analysis, name="analysis"),
+    #
+    path("<str:entity_TradingSymbol>/Arch", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/Bridge", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/Clock", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/Graph", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/Opinion", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualCashFlow", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualBalanceSheets", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualIncomeStatements", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualComprehensiveIncome", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualStockholdersEquity", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualTrialBalances", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/Mission", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/MaterialityThreshold", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/ProcedureAnnualCashFlow", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/ProcedureAnnualBalanceSheets", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/ProcedureAnnualIncomeStatements", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/ProcedureAnnualComprehensiveIncome", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/ProcedureAnnualStockholdersEquity", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AuditDetailed", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AuditSummary", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AnnualFinancialRatios", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/AdditionnalInformation", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/CapitalizedCashFlow", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/CapitalizedIncome", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/IntrinsicValues", views.analysis, name="analysis"),
+    path("<str:entity_TradingSymbol>/CapitalizationRate", views.analysis, name="analysis"),
+]
+
+urlpatterns += staticfiles_urlpatterns()
---
+++ φ/views.py
@@ -0,0 +1,91 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import *
+import datetime
+
+# ANALYSIS
+def analysis(request, entity_TradingSymbol):
+    # entity
+    entity = Entity.objects.get(TradingSymbol=entity_TradingSymbol)
+    # trial balance
+    TrialBalance_lastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="lastyear")
+    TrialBalance_secondlastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="secondlastyear")
+    TrialBalance_thirdlastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="thirdlastyear")
+    TrialBalance_fourthlastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="fourthlastyear")
+    TrialBalance_fifthlastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="fifthlastyear")
+    TrialBalance_sixthlastyear = TrialBalance.objects.get(TradingSymbol=entity_TradingSymbol, Period="sixthlastyear")
+    # cash flow
+    CashFlow_lastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="lastyear")
+    CashFlow_secondlastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="secondlastyear")
+    CashFlow_thirdlastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="thirdlastyear")
+    CashFlow_fourthlastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="fourthlastyear")
+    CashFlow_fifthlastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="fifthlastyear")
+    CashFlow_sixthlastyear = CashFlow.objects.get(TradingSymbol=entity_TradingSymbol, Period="sixthlastyear")
+    # AuditData
+    AuditData_lastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="lastyear")
+    AuditData_secondlastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="secondlastyear")
+    AuditData_thirdlastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="thirdlastyear")
+    AuditData_fourthlastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="fourthlastyear")
+    AuditData_fifthlastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="fifthlastyear")
+    AuditData_sixthlastyear = AuditData.objects.get(TradingSymbol=entity_TradingSymbol, Period="sixthlastyear")
+    #
+    return render(request, "./φ/analysis.html", {
+        #
+        "entity": entity,
+        #
+        "TrialBalance_lastyear": TrialBalance_lastyear,
+        "TrialBalance_secondlastyear": TrialBalance_secondlastyear,
+        "TrialBalance_thirdlastyear": TrialBalance_thirdlastyear,
+        "TrialBalance_fourthlastyear": TrialBalance_fourthlastyear,
+        "TrialBalance_fifthlastyear": TrialBalance_fifthlastyear,
+        "TrialBalance_sixthlastyear": TrialBalance_sixthlastyear,
+        #
+        "CashFlow_lastyear": CashFlow_lastyear,
+        "CashFlow_secondlastyear": CashFlow_secondlastyear,
+        "CashFlow_thirdlastyear": CashFlow_thirdlastyear,
+        "CashFlow_fourthlastyear": CashFlow_fourthlastyear,
+        "CashFlow_fifthlastyear": CashFlow_fifthlastyear,
+        "CashFlow_sixthlastyear": CashFlow_sixthlastyear,
+        #
+        "AuditData_lastyear": AuditData_lastyear,
+        "AuditData_secondlastyear": AuditData_secondlastyear,
+        "AuditData_thirdlastyear": AuditData_thirdlastyear,
+        "AuditData_fourthlastyear": AuditData_fourthlastyear,
+        "AuditData_fifthlastyear": AuditData_fifthlastyear,
+        "AuditData_sixthlastyear": AuditData_sixthlastyear,
+    })
+
+# DB
+def db(request):
+    return render(request, "./φ/db.html", {
+        "entities": Entity.objects.all().order_by('-Clockφ').exclude(Status='Misstated').exclude(Status='0')
+    })
+
+# QP
+def qp(request):
+    return render(request, "./φ/qp.html", {
+        "entities": Entity.objects.all().order_by('-Clockφ').exclude(Status='Audited')
+    })
+    
+# DISCLAIMER
+def disclaimer(request):
+    return render(request, "./φ/disclaimer.html")
+
+# DOCUMENTATION
+def documentation(request):
+    return render(request, "./φ/documentation.html")
+    
+# ENTITIES
+def entities(request):
+    return render(request, "./φ/entities.html", {
+        "entities": Entity.objects.all().order_by('TradingSymbol')
+    })
+    
+# MENU
+def menu(request):
+    return render(request, "./φ/menu.html")
+        
+# INDEX
+def index(request):
+    return render(request, "./φ/index.html")
+    
